c74e237f023c3543e300ebae6cf53f25367bdd17
jasimuddin <jasimuddinjm4@gmail.com>
2024-02-09 17:07:00 +0600

    student management

---
+++ resources/views/student/footer.blade.php
@@ -0,0 +1,42 @@
+ <!-- footer menu -->
+
+ <footer class="footer py-4 bg-light mt-5">
+            <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+               
+                </div>
+                <div class="col-md-4">
+                <h4>Contact Us</h4>
+                <ul class="list-unstyled">
+                    <li>
+                    <a  href="#">01998592401</a>
+                    </li>
+                    <li>
+                    <a href="#">www.jasimuddinj75@gmail.com</a>
+                    </li>
+                    <li>
+                    <a href="#">Savar, Dhaka</a>
+                    </li>
+                </ul>
+                </div>
+                <div class="col-md-4">
+                <h4>Follow Us</h4>
+                <ul class="list-unstyled">
+                    <li>
+                    <a href="https://web.facebook.com/mdjasim.uddin.10004"><i class="fab fa-facebook-square"></i> Facebook</a>
+                    </li>
+                    <li>
+                    <a href="#"><i class="fab fa-twitter-square"></i> Twitter</a>
+                    </li>
+                    <li>
+                    <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            <div class="text-center py-2">
+                <p class="mb-0">© StudentManagemanet {{date('Y')}}</p>
+            </div>
+            </div>
+        </footer>
